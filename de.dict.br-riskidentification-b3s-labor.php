7482be1ce05cf9fe71cca50b556ba1973cfaf369
rudnerbjoern <rudnerbjoern@hotmail.com>
2021-07-19 14:52:24 +0200

    Template

---
+++ de.dict.br-riskidentification-b3s-labor.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2021 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2021-04-15
+ *
+ * Localized data
+ */
+
+//
+// Typology Information
+//
+Dict::Add('DE DE', 'German', 'Deutsch', array());
+
+//
+// Empty
+//
+Dict::Add('DE DE', 'German', 'Deutsch', array());
---
+++ module.br-riskidentification-b3s-labor.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @copyright   Copyright (C) 2021 Björn Rudner
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2021-07-19
+ *
+ * iTop module definition file
+ */
+
+SetupWebPage::AddModule(
+    __FILE__, // Path to the current file, all other file names are relative to the directory containing this file
+    'br-riskidentification-b3s-labor/0.1.0',
+    array(
+        // Identification
+        //
+        'label' => 'Datamodel: B3S Labor - Riskidentification',
+        'category' => 'business',
+
+        // Setup
+        //
+        'dependencies' => array(
+            'itop-config-mgmt/2.7.1'
+        ),
+        'mandatory' => false,
+        'visible' => true,
+
+        // Components
+        //
+        'datamodel' => array(
+            'model.br-riskidentification-b3s-labor.php',
+        ),
+        'webservice' => array(),
+        'data.struct' => array(
+            // add your 'structure' definition XML files here,
+        ),
+        'data.sample' => array(
+            // add your sample data XML files here,
+        ),
+
+        // Documentation
+        //
+        'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
+        'doc.more_information' => '', // hyperlink to more information, if any
+
+        // Default settings
+        //
+        'settings' => array(
+            // Module specific settings go here, if any
+        ),
+    )
+);
